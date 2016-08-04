1c050c7ff8fdb1b2cfc234712d39a76353593806
Raymond Navarro <iwaseken06@gmail.com>
2016-08-04 17:20:18 +0800

    Finish authentication chapter

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :rumbl,
+  ecto_repos: [Rumbl.Repo]
+
+# Configures the endpoint
+config :rumbl, Rumbl.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "Dn7c/u0XzXDwE9wUwhNg5ACBk/3GDoaN3rhK3LiJKinEFLOF2+OLzDapMfVnxN1c",
+  render_errors: [view: Rumbl.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Rumbl.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
