575a260a6fd53028914e0607e67d55a435d60b06
Raymond Navarro <iwaseken06@gmail.com>
2016-08-08 17:33:15 +0800

    finish adding annotations and channel to webpage

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -1,28 +1,41 @@
 defmodule Rumbl.Auth do
 	import Plug.Conn
-	
+	import Phoenix.Controller
+	alias Rumbl.Router.Helpers
+	import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+
 	def init(opts) do
 		Keyword.fetch!(opts, :repo)
 	end
-		def call(conn, repo) do
+	def call(conn, repo) do
 		user_id = get_session(conn, :user_id)
+
 		cond do
-			user = conn.assigns[:current_user] ->
-			conn
-			user = user_id && repo.get(Rumbl.User, user_id) ->
-			assign(conn, :current_user, user)
-			true ->
-			assign(conn, :current_user, nil)
+			user = conn.assigns[:current_user] -> 
+				put_current_user(conn, user)
+			user = user_id && repo.get(Rumbl.User, user_id) -> 
+				put_current_user(conn, user)
+			true -> 
+				assign(conn, :current_user, nil)
 		end
 	end
+
 	def login(conn, user) do
 		conn
-		|> assign(:current_user, user)
+		|> put_current_user(user)
 		|> put_session(:user_id, user.id)
 		|> configure_session(renew: true)
 	end
 
-	import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+
+	defp put_current_user(conn, user) do
+		token = Phoenix.Token.sign(conn, "user socket", user.id)
+		conn
+		|> assign(:current_user, user)
+		|> assign(:user_token, token)
+	end
+
+	# import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
 	def login_by_username_and_pass(conn, username, given_pass, opts) do
 		repo = Keyword.fetch!(opts, :repo)
 		user = repo.get_by(Rumbl.User, username: username)
@@ -40,16 +53,17 @@ defmodule Rumbl.Auth do
 		configure_session(conn, drop: true)
 	end
 	
-	import Phoenix.Controller
-	alias Rumbl.Router.Helpers
-		def authenticate_user(conn, _opts) do
+	# import Phoenix.Controller
+	# alias Rumbl.Router.Helpers
+	def authenticate_user(conn, _opts) do
 		if conn.assigns.current_user do
-		conn
+			conn
 		else
-		conn
-		|> put_flash(:error, "You must be logged in to access that page")
-		|> redirect(to: Helpers.page_path(conn, :index))
-		|> halt()
+			conn
+			|> put_flash(:error, "You must be logged in to access that page")
+			|> redirect(to: Helpers.page_path(conn, :index))
+			|> halt()
+		end
 	end
-	end
+
 end
--- web/static/js/player.js
+++ web/static/js/player.js
@@ -20,7 +20,7 @@ onIframeReady(domId, playerId, onReady){
 	})
 },
 onPlayerStateChange(event){ },
-	etCurrentTime(){ return Math.floor(this.player.getCurrentTime() * 1000) },
+	getCurrentTime(){ return Math.floor(this.player.getCurrentTime() * 1000) },
 	seekTo(millsec){ return this.player.seekTo(millsec / 1000) }
 }
 export default Player
