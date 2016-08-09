ffdf2ac29dfa72bbd136f80951482af64bea1494
Raymond Navarro <iwaseken06@gmail.com>
2016-08-09 11:55:44 +0800

    Finish OTP chapter. Building wolfram info system

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -6,8 +6,8 @@ defmodule Rumbl.VideoChannel do
 	# 	{:ok, assign(socket, :video_id, String.to_integer(video_id))}
 	# end
 
-	def join("videos:" <> video_id, _params, socket) do
-		
+	def join("videos:" <> video_id, params, socket) do
+
 		last_seen_id = params["last_seen_id"] || 0
 		video_id = String.to_integer(video_id)
 		video = Repo.get!(Rumbl.Video, video_id)
@@ -45,18 +45,37 @@ defmodule Rumbl.VideoChannel do
 			|> Rumbl.Annotation.changeset(params)
 
 		case Repo.insert(changeset) do
-			{:ok, annotation} ->
-				broadcast! socket, "new_annotation", %{
-					id: annotation.id,
-					user: Rumbl.UserView.render("user.json", %{user: user}),
-					body: annotation.body,
-					at: annotation.at
-				}
-					{:reply, :ok, socket}
+			{:ok, ann} ->
+				broadcast_annotation(socket, ann)
+				Task.start_link(fn -> compute_additional_info(ann, socket) end)
+				{:reply, :ok, socket}
 			{:error, changeset} ->
 				{:reply, {:error, %{errors: changeset}}, socket}
+		end
+	end
+
+	defp broadcast_annotation(socket, annotation) do
+		annotation = Repo.preload(annotation, :user)
+		rendered_ann = Phoenix.View.render(AnnotationView, "annotation.json", %{
+			annotation: annotation
+		})
+		broadcast! socket, "new_annotation", rendered_ann
+	end
+	
+	defp compute_additional_info(ann, socket) do
+		for result <- Rumbl.InfoSys.compute(ann.body, limit: 1, timeout: 10_000) do
+			attrs = %{url: result.url, body: result.text, at: ann.at}
+			info_changeset =
+			Repo.get_by!(Rumbl.User, username: result.backend)
+			|> build_assoc(:annotations, video_id: ann.video_id)
+			|> Rumbl.Annotation.changeset(attrs)
+
+			case Repo.insert(info_changeset) do
+				{:ok, info_ann} -> broadcast_annotation(socket, info_ann)
+				{:error, _changeset} -> :ignore
 			end
 		end
+	end
 	# def handle_in("new_annotation", params, socket) do
 	# 	broadcast! socket, "new_annotation", %{
 	# 	user: %{username: "anon"},
